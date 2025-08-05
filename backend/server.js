4c24b67e720e392d979f32dbfc2ceb2536746f24
Shivanshxsharma <shivanshsharma9858@gmail.com>
2025-08-06 00:13:56 +0530

    fix: add build script for Vercel

--- backend/server.js
+++ backend/server.js
@@ -34,10 +34,7 @@ socketConnection(server);
 
 
 const port=5000;
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log("✅ MongoDB connected"))
 .catch(err => console.error("❌ MongoDB error:", err));
 
