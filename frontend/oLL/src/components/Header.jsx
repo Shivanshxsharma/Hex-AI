888ef6e97ed8b037da7a60979a5396b1266b5c42
Shivanshxsharma <shivanshsharma9858@gmail.com>
2025-08-11 06:34:23 +0530

    refactor Header component to improve headline display logic and formatting

--- frontend/oLL/src/components/Header.jsx
+++ frontend/oLL/src/components/Header.jsx
@@ -1,10 +1,16 @@
-function Header({Headline}) {
+function Header({ Headline }) {
+  const displayHeadline =
+    Headline?.toLowerCase() === "friend"
+      ? "Your Friend"
+      : `${Headline || "HEX"} ${Headline?"Expert":"AI"}`;
 
-    return (  
-        <div className="w-full flex items-center  h-10 bg-[#141414] border-b-2 border-[#2e2e2e] pl-0.5">
-         <div className='w-[95%]  h-full  flex items-center justify-center'><p>{(Headline||"HEX")+" AI"}</p></div>
-        </div>
-    );
+  return (
+    <div className="w-full flex items-center h-10 bg-[#141414] border-b-2 border-[#2e2e2e] pl-0.5">
+      <div className="w-[95%] h-full flex items-center justify-center">
+        <p>{displayHeadline}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
