ad91a17fe6fb14e112593fc0665fb354737597aa
platypus <shivanshsharma9858@gmail.com>
2026-06-27 07:08:20 +0530

    Update getKeys.js

--- backend/controllers/getKeys.js
+++ backend/controllers/getKeys.js
@@ -3,7 +3,7 @@ const User= require("../models/userData");
 async function getkeys(userId) {
     try {
         const user=await User.findOne({clerkId:userId})
-        console.log(user.apikey)
+        
         return user.apikey
     } catch (error) {
         console.log(error,"cant find any keys for the user")
